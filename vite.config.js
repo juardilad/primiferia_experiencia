c3069c90ae83a19a2d5cac6a6d4ef054418f5094
Juan Ardila <juardilad@unal.edu.co>
2025-08-21 05:56:19 -0500

    Changing viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/primiferia_experiencia/', // 👈 nombre EXACTO del repo
 })
