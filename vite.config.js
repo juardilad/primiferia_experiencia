cd99eeca353a3e1f20a33e296d7f1398346aa069
Juan Ardila <juardilad@unal.edu.co>
2025-08-21 06:03:34 -0500

    AAAAA

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/primiferia_experiencia/', // 👈 nombre EXACTO del repo
+  base: "https://juardilad.github.io/primiferia_experiencia",
 })
