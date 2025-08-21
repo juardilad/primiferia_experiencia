3b9b8622af6305f65410c385093d102a950879ed
Juan Ardila <juardilad@unal.edu.co>
2025-08-21 06:10:48 -0500

    Fix: usar HashRouter para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://juardilad.github.io/primiferia_experiencia",
+  base: '/primiferia_experiencia/',  // 👈 el nombre EXACTO de tu repo
+  build: {
+    outDir: 'dist',
+  }
 })
